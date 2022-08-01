af43c95fc2efdf23720c8958c2b7536972603d94
andy19910102 <andy19910102@gmail.com>
2022-08-02 02:17:58 +0800

    Create mutiple hash generate process using 10-million-password-list-top-500.txt

--- demo_with_10_million_password_list_top_500.py
+++ demo_with_10_million_password_list_top_500.py
@@ -1,5 +1,6 @@
 import requests
 from pw_hash import PWHash
+import csv
 
 password_txt_source_url = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-500.txt"
 
@@ -7,8 +8,15 @@ res = requests.get(password_txt_source_url)
 password_list = res.text.split()
 hash_list = []
 
-for password in password_list:
-    pw_hash = PWHash(password)
-    hash_list.append(pw_hash)
-
-print(hash_list)
+csv_header = ["Password", "Prefix Salt", "Suffix Salt", "Hash"]
+with open('countries.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(csv_header)
+    for password in password_list:
+        # Using testing mod to create PWHash Object
+        # You will be able to get the prefix, suffix and hashed_password
+        # from the PWHash Object
+        pw_hash = PWHash(password, mod="testing")
+        hash_list.append(pw_hash)
+        row = [password, pw_hash.prefix, pw_hash.suffix, pw_hash.hashed_password]
+        writer.writerow(row)
